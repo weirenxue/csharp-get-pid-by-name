bbb38348518a54fb873b2c5f5e0dc560700de397
wrxue <a0981355215@gmail.com>
2020-08-07 14:31:41 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace csharp_get_pid_by_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string processName = "notepad";
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes == null || processes.Length == 0)
+            {
+                Console.WriteLine("Process '" + processName + "' is not found.");
+                Console.ReadKey();
+                return;
+            }
+            for (var i = 0; i < processes.Length; i++)
+            {
+                Console.WriteLine("Process '" + processName + "' ID is " + processes[i].Id + ".");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# This project is about how to obtain process ID
+1. Search for processes by name(such as notepad) without extensions instead of window name(such as test.txt).
